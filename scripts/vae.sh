83f5c05a1c4c21863d5b63ef09151ded9a3482cd
Yusuke IWASAWA <iwasawa@weblab.t.u-tokyo.ac.jp>
2019-04-26 11:35:05 +0900

    add experiment configurations for vae

--- scripts/vae.sh
+++ scripts/vae.sh
@@ -29,6 +29,20 @@ zikkou() {
     eval $cuda $classify $1 "classifier.hiddens='[400]'" $finec
     eval $cuda $classify $1 "classifier.hiddens='[400]'" $finegc
 }
+
+labelsize() {
+    eval $omp $base $1
+    
+    eval $cuda $classify $1 $2 $pre
+    eval $cuda $classify $1 $2 $fine
+    eval $cuda $classify $1 $2 $finec
+    eval $cuda $classify $1 $2 $finegc
+    
+    eval $cuda $classify $1 $2 "classifier.hiddens='[400]'" $pre
+    eval $cuda $classify $1 $2 "classifier.hiddens='[400]'" $fine
+    eval $cuda $classify $1 $2 "classifier.hiddens='[400]'" $finec
+    eval $cuda $classify $1 $2 "classifier.hiddens='[400]'" $finegc
+}
 # hidden
 zikkou "method.hidden=400"
 zikkou "method.hidden=800"
@@ -36,6 +50,11 @@ zikkou "method.hidden=1600"
 zikkou "method.hidden=3200"
 # hidden
 zikkou "optim.num_batch=30000"
+
+# labelsize
+labelsize "method.hidden=1600" "classifier.label_size=0.1"
+labelsize "method.hidden=1600" "classifier.label_size=0.2"
+labelsize "method.hidden=1600" "classifier.label_size=0.4"
 
 fin=$(date)
 echo "Start: $start"
